8a51483cf6c0ffbc41cf3ab5a76b2802da891ec9
abroderick <abroderick@perimeterinstitute.ca>
2022-07-17 01:06:40 -0400

    Place holder for data preprocessing.

---
+++ ngEHTforecast/data/__init__.py
@@ -0,0 +1,15 @@
+"""
+Fisher matrix based forecasting tools.
+
+Note that data generation and preprocessing tools are found in ../data.
+"""
+
+__author__="Avery E. Broderick & Dominic W. Pesce"
+
+__all__ = ['processing']
+
+# Import all modules
+from . import *
+
+# Import module components
+from .processing import *
---
+++ ngEHTforecast/data/processing.py
@@ -0,0 +1,8 @@
+import numpy as np
+import ehtim as eh
+
+import ngEHTforecast.fisher.fisher_forecast as ff
+
+def preprocess_data(obs,ff,**kwargs) :
+    raise(RuntimeError("Not defined yet."))                  
+    return obs
